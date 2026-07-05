f01aacecc68f8e1db06b67eff9b252200161d7c0
TheGleamCleanTeamLLC <gaskintessa1@gmail.com>
2026-07-05 19:42:14 +0000

    Fix Get Free Quote button

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             <a href="#about">About</a>
             <a href="#services">Services</a>
             <a href="#gallery">Gallery</a>
-            <a href="#contact">Contact</a>
+            <a href="/book">Contact</a>
           </nav>
 
         
